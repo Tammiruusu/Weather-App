f768c2db7efe78ec07308cf395552d71962adc98
Tiia <tiiu_maria@hotmail.com>
2026-08-27 22:23:54 +0300

    Display error ja Weatherform eventlistener valmiit

--- index.js
+++ index.js
@@ -3,3 +3,47 @@
 const weatherForm = document.querySelector(".weatherForm");
 const cityInput = document.querySelector(".cityInput");
 const card = document.querySelector(".card");
+
+
+weatherForm.addEventListener("submit", async event => {
+
+    event.preventDefault();
+
+    const city = cityInput.value;
+
+    if(city){
+        try{
+            const weatherData = await getWeatherData(city);
+            displayWeatherInfo(weatherData);
+        }catch(error){
+            console.log(error);
+            displayError(error);
+        }
+    } else {
+        displayError("Kirjoita kaupunki")
+    }
+
+}); 
+
+async function getWeatherData(city) {
+    const apiURL = ``;
+}
+
+function displayWeatherInfo(data) {
+
+}
+
+function getWeatherEmoji(weatherId) {
+
+}
+
+function displayError(message) {
+
+    const errorDisplay = document.createElement("p");
+    errorDisplay.textContent = message;
+    errorDisplay.classList.add("errorDisplay");
+
+    card.textContent = "";
+    card.style.display = "flex";
+    card.appendChild(errorDisplay);
+}
