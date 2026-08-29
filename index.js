8864a00f03a808a7c88b1f530df4ac64d1e03022
Tiia <tiiu_maria@hotmail.com>
2026-08-29 14:21:03 +0300

    Weather App valmis, korjataan vain Css värit. Apiavain puuttuu.

--- index.js
+++ index.js
@@ -79,7 +79,24 @@ function displayWeatherInfo(data) {
 function getWeatherEmoji(weatherId) {
 
     switch(true){
-        case(weatherId >= 200)
+        case(weatherId >= 200 && weatherId < 300):
+            return "🌧️";
+        case(weatherId >= 300 && weatherId < 400):
+            return "🌧️";
+        case(weatherId >= 500 && weatherId < 600):
+            return "🌧️";
+        case(weatherId >= 600 && weatherId < 700):
+            return "❄️";
+        case(weatherId >= 700 && weatherId < 800):
+            return "🌫️";
+        case(weatherId === 800):
+            return "🌞";
+        case(weatherId >= 801 && weatherId < 810):
+            return "☁️";
+        default:
+            return "❓";
+        
+
     }
 
 }
